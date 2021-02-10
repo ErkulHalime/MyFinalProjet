81f2e5876f6832d5bade63fb908ad78f638bc9a3
halime <halime.software@gmail.com>
2021-02-10 23:37:29 +0300

    10 Şubat 2. commit

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,11 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min,decimal max);
-        List<ProductDetailDto> GetProductDetailDtos();
-        Product GetById(int id);
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min,decimal max);
+        IDataResult<List<ProductDetailDto>> GetProductDetailDtos();
+        IDataResult<Product> GetById(int id);
 
         IResult Add(Product product);
 
